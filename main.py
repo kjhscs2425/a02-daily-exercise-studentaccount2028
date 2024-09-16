f726da2238af9bf5e076b1441c66d57ca65c52cc
studentaccount2028 <tpark2028@kehillahstudent.org>
2024-09-16 09:08:37 -0700

    Finished project

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import turtle
+length1 =  200
+turtle.left(180)
+for i in range(6):
+    length1 = length1 - 30
+    for i in range(2):
+        turtle.forward(length1)
+        turtle.left(90)
+turtle.exitonclick()
